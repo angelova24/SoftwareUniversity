bdc02d9ad0198dd65eb66ee36c6b3324c27f9306
vilangelova vilangelova <vilangelova@students.softuni.bg>
2021-05-14 21:08:27 +0200

    Added Defining Classes exercises

---
+++ C# Advanced/05.Defining Classes/DefiningClasses/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            familyMembers=new List<Person>();
+        }
+
+
+        public void AddMember(Person member)
+        {
+            familyMembers.Add(member);
+        }
+
+        public List<Person> GetMembersOver30()
+        {
+            List<Person> persons = familyMembers.Where(p => p.Age>30).ToList();
+
+            return persons;
+        }
+    }
+}
---
+++ C# Advanced/05.Defining Classes/DefiningClasses/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] membersData = Console.ReadLine().Split();
+
+                string memberName = membersData[0];
+                int memberAge = int.Parse(membersData[1]);
+
+                Person member = new Person(memberName,memberAge);
+
+                family.AddMember(member);
+            }
+
+            var membersOver30 = family.GetMembersOver30();
+
+            foreach (var person in membersOver30.OrderBy(x=> x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
